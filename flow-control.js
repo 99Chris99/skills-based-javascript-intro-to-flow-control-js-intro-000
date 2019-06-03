62b5ea6a28efc55f67aa992844a8adda4ca253a0
Chris Hodge <chrishodge87@gmail.com>
2019-06-03 16:08:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,10 +7,10 @@ return "You are a teenager!"
 
 function teenager(age) {
 if (age >= 13 && age <= 19) {
-"You are a teenager!"
+return "You are a teenager!"
 }
 else if (age < 13 || age > 19) {
-return"You are not a teenager"
+return "You are not a teenager"
 }
 
 }
