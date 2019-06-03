31f5e5f5ff219da9f5e81fc2bcc6389786667257
Chris Hodge <chrishodge87@gmail.com>
2019-06-03 16:12:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,6 +17,16 @@ return "You are not a teenager"
 
 function ageChecker(age) {
 
+if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+}
+else if (age < 13) {
+  return "You are a kid."
+}
+else if (age > 19) {
+  return "You are a grownup"
+}
+
 }
 
 function ternaryTeenager(age) {
